c7a2b35f9dd12d5fc1e6e1c7defdcb4897c486b3
AlexanderTereshenkov <alexandertereshenkov@mail.ru>
2023-05-27 12:18:03 +0300

    UI fixed

--- app/src/main/java/com/example/orthoepy/TeachingFragment.kt
+++ app/src/main/java/com/example/orthoepy/TeachingFragment.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ListView
-import android.widget.TextView
 
 
 private const val ARG_PARAM1 = "param1"
